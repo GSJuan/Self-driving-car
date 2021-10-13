493906968d89318f4a020fc1f20e29ce9447a286
DanielHdez <65633495+alu0101331720@users.noreply.github.com>
2021-10-13 22:41:39 +0100

    Delete Self-driving-car-Interfaz-Dijkstra directory

--- Self-driving-car-Interfaz-Dijkstra/simulation.d
+++
@@ -1,4 +0,0 @@
-obj/simulation.o: src/simulation.cpp src/include/simulation.h \
- src/include/finite_world.h src/include/world.h src/include/taxi.h \
- src/include/vehicle.h src/include/uber.h src/include/vector.h \
- src/include/cell.h
